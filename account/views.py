d8f99b5087f556070104496a98fef664cddf2565
Frit027 <Frit_027@mail.ru>
2022-05-09 11:50:50 +0300

    added registration and main page views

--- account/views.py
+++ account/views.py
@@ -1,23 +1,28 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import PasswordChangingForm, UserRegistrationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordChangeView
 
 
-def user_login(request):
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    success_url = reverse_lazy('password_change_done')
+
+
+@login_required
+def main_page(request):
+    return render(request, 'account/main_page.html', {'section': 'main_page'})
+
+
+def register(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
