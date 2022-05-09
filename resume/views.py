59de804861aed759a1397851596c7913e5192ce2
Frit027 <Frit_027@mail.ru>
2022-05-09 19:23:36 +0300

    added 'is_file_loaded' flag

--- resume/views.py
+++ resume/views.py
@@ -6,13 +6,20 @@ from .models import Resume
 
 @login_required
 def main_page(request):
+    is_file_loaded = False
+
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             files = request.FILES.getlist('file_field')
             for f in files:
                 Resume(file=f, user=request.user).save()
+            is_file_loaded = True
     else:
         form = DocumentForm()
 
-    return render(request, 'resume/main_page.html', {'section': 'main_page', 'form': form})
+    return render(
+        request,
+        'resume/main_page.html',
+        {'section': 'main_page', 'form': form, 'is_file_loaded': is_file_loaded}
+    )
