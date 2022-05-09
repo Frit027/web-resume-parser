fcc90e207f9b2040dc62dd4edcc20c6bf26594e7
Frit027 <Frit_027@mail.ru>
2022-05-09 22:19:14 +0300

    added resume parser

--- resume/views.py
+++ resume/views.py
@@ -2,24 +2,38 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import DocumentForm
 from .models import Resume
+from django.core.files.base import ContentFile
+from .json_creator import JSONCreator
+from .file_converter import FileConverter
+from .utilities import get_json_filename
+import json
 
 
 @login_required
 def main_page(request):
-    is_file_loaded = False
+    is_files_loaded = False
 
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            files = request.FILES.getlist('file_field')
-            for f in files:
-                Resume(file=f, user=request.user).save()
-            is_file_loaded = True
+            converter = FileConverter()
+            json_creator = JSONCreator()
+
+            for file in request.FILES.getlist('file_field'):
+                resume = Resume(file=file, user=request.user)
+                resume.save()
+                json_str = json_creator.create_json(converter.get_text(resume.file.path))
+                resume.json.save(get_json_filename(resume.file.name), ContentFile(json_str))
+
+                with open(resume.json.path, 'r', encoding='utf-8') as f:
+                    print(json.load(f))
+
+            is_files_loaded = True
     else:
         form = DocumentForm()
 
     return render(
         request,
         'resume/main_page.html',
-        {'section': 'main_page', 'form': form, 'is_file_loaded': is_file_loaded}
+        {'section': 'main_page', 'form': form, 'is_files_loaded': is_files_loaded}
     )
