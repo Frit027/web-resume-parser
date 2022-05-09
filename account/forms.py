790d9d95022745aa5a63fd74790e7033e96c0460
Frit027 <Frit_027@mail.ru>
2022-05-09 19:19:57 +0300

    added labels

--- account/forms.py
+++ account/forms.py
@@ -4,12 +4,14 @@ from django.contrib.auth.models import User
 
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    username = forms.CharField(label='Логин')
+    first_name = forms.CharField(label='Ваше имя')
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ('username', 'first_name',)
+        fields = ('username', 'first_name')
 
     def clean_password2(self):
         cd = self.cleaned_data
@@ -19,6 +21,8 @@ class UserRegistrationForm(forms.ModelForm):
 
 
 class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Логин')
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.update(
@@ -30,6 +34,11 @@ class CustomLoginForm(AuthenticationForm):
 
 
 class PasswordChangingForm(PasswordChangeForm):
-    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    new_password1 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    new_password2 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                                   label='Старый пароль')
+    new_password1 = forms.CharField(max_length=100,
+                                    widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                                    label='Новый пароль')
+    new_password2 = forms.CharField(max_length=100,
+                                    widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                                    label='Повторите новый пароль')
