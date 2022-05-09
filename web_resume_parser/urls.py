5fc19ef8ec040e19f1f34dea616e6f5f0bf70df8
Frit027 <Frit_027@mail.ru>
2022-05-09 11:51:17 +0300

    added urls to account and main page

--- web_resume_parser/urls.py
+++ web_resume_parser/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from account import views
 
 urlpatterns = [
+    path('', views.main_page, name='main_page'),
+    path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
 ]
