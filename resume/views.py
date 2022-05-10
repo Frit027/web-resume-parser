b2fae51ce4cc14236a9cdc73eeeade1f846a5859
Frit027 <Frit_027@mail.ru>
2022-05-10 22:26:45 +0300

    form replaced to ajax

--- resume/views.py
+++ resume/views.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 from django.shortcuts import render
 from django.core.files.base import ContentFile
 from django.contrib.auth.decorators import login_required
@@ -9,6 +7,7 @@ from .models import Resume
 from .json_creator import JSONCreator
 from .file_converter import FileConverter
 from .utilities import get_json_filename
+from django.http import JsonResponse
 
 
 @login_required
@@ -38,37 +37,26 @@ def main_page(request):
         }
     )
 
+
 @login_required
 def analysis(request):
     data = Resume.get_data(request.user)
-    resumes = []
     if request.method == 'POST':
-        levels = request.POST.getlist('levels')
+        skills = ('operating_system', 'stylesheet_lang', 'version_control',
+                  'program_lang', 'markup_lang', 'sys_admin', 'frontend',
+                  'android', 'testing', 'gamedev', 'backend', 'devops',
+                  'ios', 'db', 'ml')
+        levels = request.POST.getlist('levels[]')
         age = request.POST['age']
         experience = request.POST['experience']
+        skills_dict = {}
+        for skill in skills:
+            skills_dict[skill] = request.POST.getlist(f'skills[{skill}][]')
 
-        skills = {
-            'program_lang': request.POST.getlist('program_lang'),
-            'stylesheet_lang': request.POST.getlist('stylesheet_lang'),
-            'markup_lang': request.POST.getlist('markup_lang'),
-            'db': request.POST.getlist('db'),
-            'operating_system': request.POST.getlist('operating_system'),
-            'version_control': request.POST.getlist('version_control'),
-            'ios': request.POST.getlist('ios'),
-            'android': request.POST.getlist('android'),
-            'ml': request.POST.getlist('ml'),
-            'gamedev': request.POST.getlist('gamedev'),
-            'testing': request.POST.getlist('testing'),
-            'sys_admin': request.POST.getlist('sys_admin'),
-            'devops': request.POST.getlist('devops'),
-            'frontend': request.POST.getlist('frontend'),
-            'backend': request.POST.getlist('backend')
-        }
-
-        resumes = Resume.get_resumes_by_filters(request.user, age, experience, levels, skills)
-        print(levels, reduce(lambda d, el: d.extend(el) or d, skills.values(), []))
+        resumes = Resume.get_resumes_by_filters(request.user, age, experience, levels, skills_dict)
         [print(r.data) for r in resumes]
         print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        return JsonResponse({'resumes': [resume.file.name for resume in resumes]})
     else:
         pass
-    return render(request, 'resume/analysis.html', {'data': data, 'resumes': resumes})
+    return render(request, 'resume/analysis.html', {'data': data})
