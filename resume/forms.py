05423230be91bd3261c6a3c4018c13ce47e61bf1
Frit027 <Frit_027@mail.ru>
2022-05-10 20:06:23 +0300

    renamed constant

--- resume/forms.py
+++ resume/forms.py
@@ -4,18 +4,17 @@ import os
 
 
 class DocumentForm(forms.ModelForm):
+    __ALLOWED_TYPES = ('PDF', 'DOCX')
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
     class Meta:
         model = Resume
         fields = ('file_field',)
 
-    ALLOWED_TYPES = ('PDF', 'DOCX')
-
     def clean(self):
         for file in self.files.getlist('file_field'):
             extension = os.path.splitext(file.name)[1][1:].upper()
-            if extension in self.ALLOWED_TYPES:
+            if extension in self.__ALLOWED_TYPES:
                 return file
             else:
-                raise forms.ValidationError(f"Разрешены файлы форматов {', '.join(self.ALLOWED_TYPES)}.")
+                raise forms.ValidationError(f"Разрешены файлы форматов {', '.join(self.__ALLOWED_TYPES)}.")
