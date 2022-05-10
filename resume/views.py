db6621754f88e3966af6209d50a4f7eb7cfa979b
Frit027 <Frit_027@mail.ru>
2022-05-10 20:01:36 +0300

    added view for analysis

--- resume/views.py
+++ resume/views.py
@@ -1,20 +1,21 @@
+from functools import reduce
+
 from django.shortcuts import render
+from django.core.files.base import ContentFile
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from .forms import DocumentForm
 from .models import Resume
-from django.core.files.base import ContentFile
 from .json_creator import JSONCreator
 from .file_converter import FileConverter
 from .utilities import get_json_filename
-import json
 
 
 @login_required
 def main_page(request):
-    is_files_loaded = False
-
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
+
         if form.is_valid():
             converter = FileConverter()
             json_creator = JSONCreator()
@@ -22,18 +23,52 @@ def main_page(request):
             for file in request.FILES.getlist('file_field'):
                 resume = Resume(file=file, user=request.user)
                 resume.save()
-                json_str = json_creator.create_json(converter.get_text(resume.file.path))
+                json_str = json_creator.get_json(converter.get_text(resume.file.path))
                 resume.json.save(get_json_filename(resume.file.name), ContentFile(json_str))
-
-                with open(resume.json.path, 'r', encoding='utf-8') as f:
-                    print(json.load(f))
-
-            is_files_loaded = True
+            return HttpResponseRedirect('/analysis/')
     else:
         form = DocumentForm()
 
     return render(
         request,
         'resume/main_page.html',
-        {'section': 'main_page', 'form': form, 'is_files_loaded': is_files_loaded}
+        {
+            'section': 'main_page',
+            'form': form
+        }
     )
+
+@login_required
+def analysis(request):
+    data = Resume.get_data(request.user)
+    resumes = []
+    if request.method == 'POST':
+        levels = request.POST.getlist('levels')
+        age = request.POST['age']
+        experience = request.POST['experience']
+
+        skills = {
+            'program_lang': request.POST.getlist('program_lang'),
+            'stylesheet_lang': request.POST.getlist('stylesheet_lang'),
+            'markup_lang': request.POST.getlist('markup_lang'),
+            'db': request.POST.getlist('db'),
+            'operating_system': request.POST.getlist('operating_system'),
+            'version_control': request.POST.getlist('version_control'),
+            'ios': request.POST.getlist('ios'),
+            'android': request.POST.getlist('android'),
+            'ml': request.POST.getlist('ml'),
+            'gamedev': request.POST.getlist('gamedev'),
+            'testing': request.POST.getlist('testing'),
+            'sys_admin': request.POST.getlist('sys_admin'),
+            'devops': request.POST.getlist('devops'),
+            'frontend': request.POST.getlist('frontend'),
+            'backend': request.POST.getlist('backend')
+        }
+
+        resumes = Resume.get_resumes_by_filters(request.user, age, experience, levels, skills)
+        print(levels, reduce(lambda d, el: d.extend(el) or d, skills.values(), []))
+        [print(r.data) for r in resumes]
+        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    else:
+        pass
+    return render(request, 'resume/analysis.html', {'data': data, 'resumes': resumes})
--- web_resume_parser/urls.py
+++ web_resume_parser/urls.py
@@ -6,5 +6,6 @@ from resume import views
 urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('account/', include('account.urls')),
+    path('analysis/', views.analysis, name='analysis'),
     path('admin/', admin.site.urls),
 ]
