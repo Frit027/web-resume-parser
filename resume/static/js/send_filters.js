134482f7967e4d801849d771d54a66ad59e4ab98
Frit027 <Frit_027@mail.ru>
2022-05-13 00:31:46 +0300

    added function for show/hide info

--- resume/static/js/send_filters.js
+++ resume/static/js/send_filters.js
@@ -1,3 +1,8 @@
+const iconClasses = {
+    caretUp: 'bi bi-caret-up-fill',
+    caretDown: 'bi bi-caret-down-fill',
+};
+
 (() => {
     $('#apply_button').click(() => {
         $.ajax({
@@ -45,33 +50,49 @@ insertInfo = (response) => {
             academies: resume.education.academies,
             experience: resume.experience,
         };
-        let ul = '';
 
-        if (Object.keys(info).some((key) => info[key])) {
-            ul = `
-                <ul>
-                  ${info.name       ? `<li>Имя: ${info.name}</li>` : ''}
-                  ${info.age        ? `<li>Возраст: ${info.age} ${plural(info.age)}</li>` : ''}
-                  ${info.email      ? `<li>Email: ${info.email}</li>` : ''}
-                  ${info.phone      ? `<li>Телефон: ${info.phone}</li>` : ''}
-                  ${info.academies  ? `<li>Оконченные учебные заведения:</li>
-                                       <ul>${info.academies.reduce((str, academy) => str + `<li>${academy}</li>`, '')}</ul>`
-                                    : ''
-                  }
-                  ${info.experience ? `<li>Опыт работы: ${info.experience} ${plural(parseInt(info.experience))}</li>` : ''}
-                </ul>
-            `;
-        }
         $('#resumes').append(`
-            <li><b>${response.filenames[i]}</b></li>
-            ${ul}
+            <div class="filename">${response.filenames[i]}</div>
+            <span class="detail" id="detail_button_${i}">Подробнее <i id="bi_${i}" class="${iconClasses.caretDown}"></i></span>
+            ${getUlInfo(info, i)}
         `);
+
+        $(`#detail_button_${i}`).on('click', { i }, toggleInfo);
         i += 1;
     })
 };
 
-plural = (number) => {
+toggleInfo = (event) => {
+    const info = $(`#info_${event.data.i}`);
+    if(info.is(':hidden')) {
+        $(`#bi_${event.data.i}`).replaceWith(`<i id="bi_${event.data.i}" class="${iconClasses.caretUp}"></i>`)
+        info.show('slow');
+    } else {
+        $(`#bi_${event.data.i}`).replaceWith(`<i id="bi_${event.data.i}" class="${iconClasses.caretDown}"></i>`)
+        info.hide('slow');
+    }
+};
+
+getWordByYear = (number) => {
     const words = ['год', 'года', 'лет'];
     const cases = [2, 0, 1, 1, 1, 2];
     return words[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
 };
+
+getUlInfo = (info, i) => {
+    if (!Object.keys(info).some((key) => info[key])) return '';
+    return `
+        <ul class="resume-info" id="info_${i}">
+          ${info.name       ? `<li>Имя: ${info.name}</li>` : ''}
+          ${info.age        ? `<li>Возраст: ${info.age} ${getWordByYear(info.age)}</li>` : ''}
+          ${info.email      ? `<li>Email: ${info.email}</li>` : ''}
+          ${info.phone      ? `<li>Телефон: ${info.phone}</li>` : ''}
+          ${info.academies  ? `<li>Оконченные учебные заведения:</li>
+                               <ul>${info.academies.reduce((str, academy) => str + `<li>${academy}</li>`, '')}</ul>`
+                            : ''
+          }
+          ${info.experience ? `<li>Опыт работы: ${info.experience} ${getWordByYear(parseInt(info.experience))}</li>` : ''}
+        </ul>
+    `;
+};
+
