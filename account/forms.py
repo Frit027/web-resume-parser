e976cbc5b3f1bc73c3e717d60e25261132eca2dc
Frit027 <Frit_027@mail.ru>
2022-05-13 14:03:05 +0300

    customize login form

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, UsernameField
 from django.contrib.auth.models import User
 
 
@@ -21,16 +21,13 @@ class UserRegistrationForm(forms.ModelForm):
 
 
 class CustomLoginForm(AuthenticationForm):
-    username = forms.CharField(label='Логин')
+    username = UsernameField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'id': 'login', 'placeholder': 'Логин'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'id': 'password', 'placeholder': 'Пароль'}))
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update(
-            {'class': 'form-control'}
-        )
-        self.fields['password'].widget.attrs.update(
-            {'class': 'form-control'}
-        )
+        super(CustomLoginForm, self).__init__(*args, **kwargs)
 
 
 class PasswordChangingForm(PasswordChangeForm):
