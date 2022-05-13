31000e73f85c5fec4ba4c46bc373791740bdb527
Frit027 <Frit_027@mail.ru>
2022-05-13 14:01:57 +0300

    renamed files

---
+++ resume/static/js/analysis.js
@@ -0,0 +1,28 @@
+(() => {
+    $('#remove_button').click(() => {
+        $.ajax({
+            type: 'POST',
+            headers: { 'X-CSRFToken': Cookies.get('csrftoken') },
+            url: '',
+            data: { is_remove_files: true },
+            success: (data) => {
+                if (data.is_removed) {
+                    $('.ul_filenames').replaceWith('<p>Вы пока не загрузили резюме.</p>');
+                    $('#remove_button').remove();
+                    $('.detail').remove();
+                }
+            }
+        });
+    });
+
+    $('.detail').click(() => {
+        const ulFilenames = $('.ul_filenames');
+        if(ulFilenames.is(':hidden')) {
+            $('.detail').html('Скрыть <i class="bi bi-caret-up-fill"></i>');
+            ulFilenames.show('slow');
+        } else {
+            $('.detail').html('Показать <i class="bi bi-caret-down-fill"></i>');
+            ulFilenames.hide('slow');
+        }
+    });
+})();
