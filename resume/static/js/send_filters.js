c2faa290093344dbbe16ed8f78fac871a6caf2f3
Frit027 <Frit_027@mail.ru>
2022-05-14 14:48:18 +0300

    renamed functions

--- resume/static/js/send_filters.js
+++ resume/static/js/send_filters.js
@@ -42,7 +42,7 @@ insertInfo = (response) => {
     $('#resumes').html('');
     let i = 0;
     response.resumes.forEach((resume) => {
-        const info = {
+        const generalInfo = {
             name: resume.name,
             age: resume.age,
             email: resume.email,
@@ -50,11 +50,12 @@ insertInfo = (response) => {
             academies: resume.education.academies,
             experience: resume.experience,
         };
+        const skills = resume.skills;
 
         $('#resumes').append(`
             <div class="filename">${response.filenames[i]}</div>
             <span class="detail" id="detail_button_${i}">Подробнее <i id="bi_${i}" class="${iconClasses.caretDown}"></i></span>
-            ${getUlInfo(info, i)}
+            ${anyKey(generalInfo) ? getGeneralInfo(generalInfo, i) : ''}
         `);
 
         $(`#detail_button_${i}`).on('click', { i }, toggleInfo);
@@ -65,10 +66,10 @@ insertInfo = (response) => {
 toggleInfo = (event) => {
     const info = $(`#info_${event.data.i}`);
     if(info.is(':hidden')) {
-        $(`#bi_${event.data.i}`).replaceWith(`<i id="bi_${event.data.i}" class="${iconClasses.caretUp}"></i>`)
+        $(`#detail_button_${event.data.i}`).html(`Скрыть <i id="bi_${event.data.i}" class="${iconClasses.caretUp}"></i>`);
         info.show('slow');
     } else {
-        $(`#bi_${event.data.i}`).replaceWith(`<i id="bi_${event.data.i}" class="${iconClasses.caretDown}"></i>`)
+        $(`#detail_button_${event.data.i}`).html(`Подробнее <i id="bi_${event.data.i}" class="${iconClasses.caretDown}"></i>`);
         info.hide('slow');
     }
 };
@@ -79,20 +80,23 @@ getWordByYear = (number) => {
     return words[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
 };
 
-getUlInfo = (info, i) => {
-    if (!Object.keys(info).some((key) => info[key])) return '';
+getGeneralInfo = (info, i) => {
     return `
-        <ul class="resume-info" id="info_${i}">
-          ${info.name       ? `<li>Имя: ${info.name}</li>` : ''}
-          ${info.age        ? `<li>Возраст: ${info.age} ${getWordByYear(info.age)}</li>` : ''}
-          ${info.email      ? `<li>Email: ${info.email}</li>` : ''}
-          ${info.phone      ? `<li>Телефон: ${info.phone}</li>` : ''}
-          ${info.academies  ? `<li>Оконченные учебные заведения:</li>
-                               <ul>${info.academies.reduce((str, academy) => str + `<li>${academy}</li>`, '')}</ul>`
-                            : ''
-          }
-          ${info.experience ? `<li>Опыт работы: ${info.experience} ${getWordByYear(parseInt(info.experience))}</li>` : ''}
-        </ul>
+        <div class="resume-info" id="info_${i}">
+            <div>Общая информация</div>
+            <ul>
+              ${info.name       ? `<li>Имя: ${info.name}</li>` : ''}
+              ${info.age        ? `<li>Возраст: ${info.age} ${getWordByYear(info.age)}</li>` : ''}
+              ${info.email      ? `<li>Email: ${info.email}</li>` : ''}
+              ${info.phone      ? `<li>Телефон: ${info.phone}</li>` : ''}
+              ${info.academies  ? `<li>Оконченные учебные заведения:</li>
+                                   <ul>${info.academies.reduce((str, academy) => str + `<li>${academy}</li>`, '')}</ul>`
+                                : ''
+              }
+              ${info.experience ? `<li>Опыт работы: ${info.experience} ${getWordByYear(parseInt(info.experience))}</li>` : ''}
+            </ul>
+        </div>
     `;
 };
 
+anyKey = (info) => Object.keys(info).some((key) => info[key]);
