fef962634d538993a2d4cc5d21d050c872fd40d4
Frit027 <Frit_027@mail.ru>
2022-05-15 14:37:07 +0300

    improved code readability

--- resume/static/js/send_filters.js
+++ resume/static/js/send_filters.js
@@ -3,11 +3,13 @@ const iconClasses = {
     caretDown: 'bi bi-caret-down-fill',
 };
 
-const SKILLS = ['Языки программирования', 'Языки описания внешнего вида страницы',
-                'Языки разметки', 'СУБД', 'Операционные системы', 'Системы контроля версий',
-                'Технологии IOS', 'Технологии Android', 'ML, Data Science/Analysis',
-                'GameDev', 'Технологии и методы тестирования', 'Администрирование',
-                'DevOps', 'Технологии Frontend-разработки', 'Технологии Backend-разработки'];
+const SKILLS = [
+    'Языки программирования', 'Языки описания внешнего вида страницы',
+    'Языки разметки', 'СУБД', 'Операционные системы', 'Системы контроля версий',
+    'Технологии IOS', 'Технологии Android', 'ML, Data Science/Analysis',
+    'GameDev', 'Технологии и методы тестирования', 'Администрирование',
+    'DevOps', 'Технологии Frontend-разработки', 'Технологии Backend-разработки'
+];
 
 (() => {
     $('#apply_button').click(() => {
@@ -23,37 +25,36 @@ const SKILLS = ['Языки программирования', 'Языки оп�
 
 getSelected = () => ({
     experience: $('#experience').find(':selected').val(),
-    age: $('#age').find(':selected').val(),
-    levels: $('#levels').val(),
+    age:        $('#age').find(':selected').val(),
+    levels:     $('#levels').val(),
     skills: {
-        program_lang: $('#program_lang').val(),
-        stylesheet_lang: $('#stylesheet_lang').val(),
-        markup_lang: $('#markup_lang').val(),
-        db: $('#db').val(),
+        program_lang:     $('#program_lang').val(),
+        stylesheet_lang:  $('#stylesheet_lang').val(),
+        markup_lang:      $('#markup_lang').val(),
+        db:               $('#db').val(),
         operating_system: $('#operating_system').val(),
-        version_control: $('#version_control').val(),
-        ios: $('#ios').val(),
-        android: $('#android').val(),
-        ml: $('#ml').val(),
-        gamedev: $('#gamedev').val(),
-        testing: $('#testing').val(),
-        sys_admin: $('#sys_admin').val(),
-        devops: $('#devops').val(),
-        frontend: $('#frontend').val(),
-        backend: $('#backend').val(),
+        version_control:  $('#version_control').val(),
+        ios:              $('#ios').val(),
+        android:          $('#android').val(),
+        ml:               $('#ml').val(),
+        gamedev:          $('#gamedev').val(),
+        testing:          $('#testing').val(),
+        sys_admin:        $('#sys_admin').val(),
+        devops:           $('#devops').val(),
+        frontend:         $('#frontend').val(),
+        backend:          $('#backend').val(),
     }}
 );
 
 insertInfo = (response) => {
     $('#resumes').html('');
-    let i = 0;
-    response.resumes.forEach((resume) => {
+    response.resumes.forEach((resume, i) => {
         const generalInfo = {
-            name: resume.name,
-            age: resume.age,
-            email: resume.email,
-            phone: resume.phone,
-            academies: resume.education.academies,
+            name:       resume.name,
+            age:        resume.age,
+            email:      resume.email,
+            phone:      resume.phone,
+            academies:  resume.education.academies,
             experience: resume.experience,
         };
         const skills = resume.skills;
@@ -68,7 +69,6 @@ insertInfo = (response) => {
         `);
 
         $(`#detail_button_${i}`).on('click', { i }, toggleInfo);
-        i += 1;
     })
 };
 
@@ -85,23 +85,24 @@ toggleInfo = (event) => {
 
 getGeneralInfo = (info) => {
     return `
-        <div class="subtitle">Общая информация</div>
-        <ul>
-          ${info.name       ? `<li><span class="text-primary">Имя:</span> ${info.name}</li>` : ''}
-          ${info.age        ? `<li><span class="text-primary">Возраст:</span> ${info.age} ${getWordByYear(info.age)}</li>` : ''}
-          ${info.email      ? `<li><span class="text-primary">Email:</span> ${info.email}</li>` : ''}
-          ${info.phone      ? `<li><span class="text-primary">Телефон:</span> ${info.phone}</li>` : ''}
-          ${info.academies.length  ? `<li><span class="text-primary">Оконченные учебные заведения:</span></li>
-                                      <ul>${info.academies.reduce((str, academy) => str + `<li>${academy}</li>`, '')}</ul>`
-                                   : ''}
-          ${info.experience ? `<li><span class="text-primary">Опыт работы:</span> ${info.experience} ${getWordByYear(parseInt(info.experience))}</li>` : ''}
-        </ul>
+      <div class="subtitle">Общая информация</div>
+      <ul>
+        ${info.name       ? `<li><span class="text-primary">Имя:</span> ${info.name}</li>` : ''}
+        ${info.age        ? `<li><span class="text-primary">Возраст:</span> ${info.age} ${getWordByYear(info.age)}</li>` : ''}
+        ${info.email      ? `<li><span class="text-primary">Email:</span> ${info.email}</li>` : ''}
+        ${info.phone      ? `<li><span class="text-primary">Телефон:</span> ${info.phone}</li>` : ''}
+        ${info.academies.length ? `<li><span class="text-primary">Оконченные учебные заведения:</span></li>
+                                 <ul>${info.academies.reduce((str, academy) => str + `<li>${academy}</li>`, '')}</ul>`
+                                : ''}
+        ${info.experience ? `<li><span class="text-primary">Опыт работы:</span> ${info.experience} ${getWordByYear(parseInt(info.experience))}</li>` : ''}
+      </ul>
     `;
 };
 
 getSkills = (obj) => {
     const lis = Object.keys(obj).reduce((res, key, i) =>
-        obj[key].length ? res + `<li><span class="text-primary">${SKILLS[i]}:</span> ${obj[key].join(', ')}</li>` : res + '', ''
+        obj[key].length ? res + `<li><span class="text-primary">${SKILLS[i]}:</span> ${obj[key].join(', ')}</li>`
+                        : res + '', ''
     );
     return `
       <div class="subtitle">Информационные технологии</div>
